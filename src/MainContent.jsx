b1821384524b5c8b6c5f4a4d1ebf588e535e1a26
rocklessg <abdulhafiz.suleiman@decagon.dev>
2021-11-04 01:49:39 +0100

    populating tables

--- src/MainContent.jsx
+++ src/MainContent.jsx
@@ -5,10 +5,43 @@ class MainContent extends Component
     constructor() {
         super()
         this.state = {pageTitle: "Customer",
-                      customersCount: 5        
-        };
-        this.onRefreshClick = this.onRefreshClick.bind(this);
-    }
+                      customersCount: 5,
+                      customers: [
+                          {
+                           id: 1, 
+                           name: "John",
+                           phone: "123-456-4566",
+                           address: {city: "Abuja"}
+                          },
+                          {
+                            id: 2, 
+                            name: "Smith", 
+                            phone: "663-556-4876",
+                            address: {city: "New Olen"}
+                          },
+                          {
+                            id: 3, 
+                            name: "Allen",
+                            phone: "093-498-4676",
+                            address: {city: "Berlin"}    
+                        },
+                            
+                          {
+                            id: 4, 
+                            name: "Jamoo",
+                            phone: "098-498-4596",
+                            address: {city: "London"}
+                          },
+                          {
+                            id: 5,
+                            name: "Scott",
+                            phone: "109-409-4096",
+                            address: {city: "New Jersey"}
+                        }
+                      ]       
+                     };
+    };
+
     render()
     {
         return(
@@ -20,6 +53,30 @@ class MainContent extends Component
                     </span>
                     <button className="btn btn-info" onClick={this.onRefreshClick}>Add Customer</button>
                 </h1>
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Customer Name</th>
+                            <th>Phone</th>
+                            <th>City</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.state.customers.map((customer) => {
+                            return (
+                            <tr key="{customer.id}">
+                                <td>{customer.id}</td>
+                                <td>{customer.name}</td>
+                                <td>{customer.phone}</td>
+                                <td>{customer.address.city}</td>
+                            </tr>
+                        
+                            );
+                        })}
+                        
+                    </tbody>
+                </table>
             </div>
         );
     }
